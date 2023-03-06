dbb8c91cab6f4feae7d6e20f2153dc0005fb4e57
Lostmonkey <xys-tc@hotmail.com>
2023-03-06 18:01:36 +0800

    init

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,9 +1,26 @@
+import logging
+
 from datetime import datetime
 from flask import render_template, request
 from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import uuid
+import json
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+logger = logging.getLogger('log')
+logging.basicConfig(level=logging.DEBUG)
+
+
+VolcAuth = "cNA03Z6qXenOhzDkGpc93PZETDZvoWAC"
+
+wxAppId = "wx022394022eafe29b"
+wxSecret = "025560721342e9401bccd561f1f2676f"
+
+GptResult = {}
 
 
 @app.route('/')
@@ -64,3 +81,61 @@ def get_count():
     """
     counter = Counters.query.filter(Counters.id == 1).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+
+@app.route("/api/volc/token")
+def get_token():
+    ret = {
+        "auth": VolcAuth,
+        "req_id": uuid.uuid4().hex
+    }
+    return json.dumps(ret)
+
+
+@app.route("/api/wechat/openid")
+def get_openid():
+    logger.info(request.headers)
+    ret = {
+        "openid": request.headers["X-WX-OPENID"],
+    }
+    return json.dumps(ret)
+
+
+executor = ThreadPoolExecutor(2)
+
+
+def submit_answer_2_gpt(req_id, param):
+    ret = requests.get("http://120.79.242.200:5088/teacher", json=param, timeout=30000)
+    GptResult[req_id] = ret.text
+
+
+@app.route("/api/chatgpt/answer", methods=["POST"])
+def submit_answer():
+    sys = "You are a professional TOEFL speaking test examiner. Please rate my answer according to official TOEFL Speaking rubrics, and give me a Totol score as well as a  breakdown chart of my speaking score.Please also give me the detailed analysis why I got the above score.  The analysis should include mistakes that I have made in my response as well as places that I could have paid more attention to or improve.Please in the end give me a sample response of the topic. This sample response should be carefully structured with detailed examples and meet the official TOEFL speaking rubrics with exam full marks."
+    req_id = request.json["req_id"]
+    ques = request.json["question"]
+    ans = request.json["answer"]
+
+    param = {
+        "req_id": req_id,
+        "system": sys,
+        "user": "The Question is: \"" + ques + "\", the student's answer is: \"" + ans + "\""
+    }
+
+    print(json.dumps(param))
+
+    GptResult.pop(req_id, None)
+
+    executor.submit(submit_answer_2_gpt, req_id, param)
+
+    return json.dumps(param)
+
+
+@app.route("/api/chatgpt/eval", methods=["GET"])
+def get_eval():
+    req_id = request.args["req_id"]
+
+    if req_id in GptResult:
+        return json.dumps({"status": 0, "data": GptResult[req_id]})
+    else:
+        return json.dumps({"status": 1000, "data": ""})
